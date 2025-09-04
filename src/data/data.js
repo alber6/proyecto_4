1a471c3220ef3685ed4d0a030ee672bd14c57d67
alber6 <albertopeinado08@gmail.com>
2025-09-04 17:36:50 +0200

    path pictures

--- src/data/data.js
+++ src/data/data.js
@@ -2,7 +2,7 @@ const data = {
   name: "Peter Parker",
   address: "123 Main Street, New York, NY",
   email: "peter.parker@email.com",
-  avatar: "/public/images/cara.png",
+  avatar: "/images/cara.png",
   aboutMe:
     "I'm a passionate web development student currently finishing my first year of a master degree. This portfolio is a showcase of the projects and skills I’ve been developing during my studies. My goal is to continue growing as a web developer, taking on new challenges and applying creative solutions. I invite you to explore the projects I’ve worked on so far.",
 
@@ -63,35 +63,35 @@ const data = {
   projects: [
     {
       title: "Photos app",
-      preview:"/public/images/photosApp.png",
+      preview:"/images/photosApp.png",
       description:"",
       link: "https://proyecto-3-iota.vercel.app/",
     },
     {
       title: "My first portfolio",
-      preview: "/public/images/myportfolio.png",
+      preview: "/images/myportfolio.png",
       description: "",
       link: "https://my-portfolio-bice-ten-73.vercel.app/",
     },  {
       title: "Olimpiadas",
-      preview:"/public/images/olimpiadas.png",
+      preview:"/images/olimpiadas.png",
       description:"",
       link: "https://olimpiadas-mu-azure.vercel.app/",
     },
     {
       title: "Gestor de libros",
-      preview: "/public/images/gestorLibro.png",
+      preview: "/images/gestorLibro.png",
       description: "XML",
       link: "http://127.0.0.1:5500/index.html",
     },  {
       title: "Proyectos de 6ª E.P.",
-      preview:"/public/images/sexto.png",
+      preview:"/images/sexto.png",
       description:"",
       link: "http://trabajos-sexto.vercel.app/",
     },
     {
       title: "Jedi Survivor",
-      preview: "/public/images/jediProject.png",
+      preview: "/images/jediProject.png",
       description: "",
       link: "https://jedisurvivor.vercel.app/",
     }
