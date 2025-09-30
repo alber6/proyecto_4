8289e95048079dbed35ebcab4668f0df65902911
alber6 <albertopeinado08@gmail.com>
2025-09-30 12:37:35 +0200

    donde main.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -25,7 +25,7 @@ const Nav = (links) => `
         </li>
         `).join("")}
         <li>
-          <a href="/cv.html" class="cv-link" target="_blank">CV</a>
+          <a href="/src/cv.html" class="cv-link" target="_blank">CV</a>
         </li>
          <li>
         <button id="themeBtn">☀</button>
